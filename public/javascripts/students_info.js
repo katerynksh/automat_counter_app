85116a7a47443aebbcb8ac3ae29847e57ab2fbdc
Sherepera Kateryna <katerynasherepera@gmail.com>
2026-01-19 10:53:09 +0200

    .2

--- public/javascripts/students_info.js
+++ public/javascripts/students_info.js
@@ -9,6 +9,7 @@ const yearOfStudyField = document.querySelector('#year-of-study');
 const additionalInfoField = document.querySelector('#additional-info');
 
 
+
 const fs = require('fs');
 // import fs from 'fs';
 findButton.onclick = function() {
